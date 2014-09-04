affd0a0830d50b0ecc687baa68a537f4e27593e5
Aurilux <primordias@gmail.com>
2014-09-04 16:19:42 -0700

    Oops, missed a version number

--- common/com/aurilux/xar/lib/XARModInfo.java
+++ common/com/aurilux/xar/lib/XARModInfo.java
@@ -4,7 +4,7 @@ public class XARModInfo {
 	public static final String MOD_ID       = "xthuoth";
 	public static final String MOD_NAME     = "Xth'uoth";
 	public static final String MOD_TITLE    = MOD_NAME + ": The Aberrant Realm";
-	public static final String MOD_VERSION  = "0.1.4";
+	public static final String MOD_VERSION  = "1.0.0";
     public static final String MOD_DEPEND   = "required-after:Thaumcraft";
 	
 	public static final String VERSION_FILE = "https://raw.githubusercontent.com/Aurilux/Xth-uoth/master/version.xml";
